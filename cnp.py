7c14f656b795e203bef60934fb0a441227c60b99
ShakibCodes <shakibonit@gmail.com>
2026-01-19 20:42:59 +0530

    First Commit

---
+++ cnp.py
@@ -0,0 +1,30 @@
+import subprocess
+import sys
+
+def run_command(command):
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError:
+        print("❌ Command failed:", " ".join(command))
+        sys.exit(1);
+
+
+def main():
+    print("🚀 Running cnp (Commit and Push)")
+    run_command(["git", "add", "."])
+
+    message = input("Enter your commit message: ").strip()
+
+
+    if not message:
+        print("❌ Commit message cannot be empty")
+        sys.exit(1)
+
+    run_command(["git", "commit", "-m", message])
+
+    run_command(["git", "push", "-m", "origin", "main"])
+
+    print("✅ Done!")
+
+if __name__ == "__main__":
+    main()
