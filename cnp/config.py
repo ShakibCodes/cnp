d3d7a9a2cb79a48c3f9fdcd426eb3c332e56b0b6
ShakibCodes <shakibonit@gmail.com>
2026-01-27 00:39:52 +0530

    update config file done

--- cnp/config.py
+++ cnp/config.py
@@ -6,3 +6,14 @@ CONFIG_FILE = os.path.join(CONFIG_DIR, "config.json")
 
 def ensure_config_dir():
     os.makedirs(CONFIG_DIR, exist_ok=True)
+
+def save_config(data):
+    ensure_config_dir()
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(data, f, indent=2)
+
+def load_config():
+    if not os.path.exists(CONFIG_FILE):
+        return None
+    with open(CONFIG_FILE, "r") as f:
+        return json.load(f)
